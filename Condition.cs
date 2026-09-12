0fd9535a12c97a264b13d0cc4dfdfae1820500d3
nikitachand97 <cnikita490@gmail.com>
2026-09-12 17:38:41 +0530

    Implement player condition checks in Condition.cs
    Fix syntax errors and improve dungeon entry logic.

---
+++ Condition.cs
@@ -0,0 +1,36 @@
+using System;
+class Program
+{
+  static void Main (string[]args)
+  {
+    int coins = 100;
+    int health = 80;
+    int potion = 3;
+    int level = 10;
+    bool hasMagicSword = true;
+     Console.WriteLine("Coins : " + coins);
+    Console.WriteLine("Health : " + health);
+    Console.WriteLine("Potion : " + potion);
+    Console.WriteLine("Level : " + level);
+    if(coins>80 && level>5 && health>0 )
+    {
+      Console.WriteLine("Player can enter the dumgeon"):
+    }
+    else
+    {
+      Console.WriteLine("Player cannot enter the dungeon");
+                        }
+                        if(health<50 || portion==0)
+                        {
+                          Console.WriteLine("Player needs help");
+                        }
+                        if(hasMagicSword)
+                        {
+                          Console.WriteLine("Magic sword equipped....);
+                                            }
+                                            }
+                                            }
+
+                          
+                        
+    
