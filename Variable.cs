7d81829caaab496f4cc0127e24394a8ed083f373
nikitachand97 <cnikita490@gmail.com>
2026-09-12 07:33:04 +0530

    Add Variable.cs with basic game stats display

---
+++ Variable.cs
@@ -0,0 +1,13 @@
+using System;
+class Program 
+{
+  static void Main (string[]args)
+  {
+   int coins = 80;
+     int health = 90;
+  int potion = 2;
+  Console.WriteLine("Coins :  "+ coins );
+  Console.WriteLine("Health : "+ health );
+  Console.WriteLine("Potion : "+ potion);
+}
+}
